bb6b5286a8f3d18437442fb9dc9de514a4c2abf8
Thur <daviarthursistro08@gmail.com>
2023-10-11 12:04:01 -0300

    aplicativoCobrinha

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,251 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="game-container">
+    <canvas id="gameCanvas" width="400" height="400"></canvas>
+    <div class="controls">
+      <table class="cross-controls">
+        <tr>
+          <td></td>
+          <td><button id="upButton" @click="moveUp">↑</button></td>
+          <td></td>
+        </tr>
+        <tr>
+          <td><button id="leftButton" @click="moveLeft">←</button></td>
+          <td></td>
+          <td><button id="rightButton" @click="moveRight">→</button></td>
+        </tr>
+        <tr>
+          <td></td>
+          <td><button id="downButton" @click="moveDown">↓</button></td>
+          <td></td>
+        </tr>
+      </table>
+    </div>
+    <div class="controls">
+      <button id="startButton" @click="startGame">Start</button>
+      <button id="pauseButton" @click="pauseGame">Pause</button>
+      <button id="restartButton" @click="restartGame">Restart</button>
+    </div>
+    <div class="score">
+      Score: {{ score }}
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  data() {
+    return {
+      canvas: null,
+      ctx: null,
+      tileSize: 20,
+      snake: [{ x: 10, y: 10 }],
+      food: { x: 5, y: 5 },
+      dx: 0,
+      dy: -1,
+      score: 0,
+      gameInterval: null,
+      gameStarted: false,
+      gamePaused: false,
+    };
+  },
+  mounted() {
+    this.canvas = document.getElementById("gameCanvas");
+    this.ctx = this.canvas.getContext("2d");
+    this.startGame();
+  },
+  methods: {
+    startGame() {
+      if (!this.gameStarted) {
+        this.gameStarted = true;
+        this.gameInterval = setInterval(this.updateGame, 100);
+      } else if (this.gamePaused) {
+        this.gamePaused = false;
+        this.gameInterval = setInterval(this.updateGame, 100);
+      }
+    },
+    pauseGame() {
+      if (this.gameInterval) {
+        clearInterval(this.gameInterval);
+        this.gamePaused = true;
+      }
+    },
+    restartGame() {
+      this.snake = [{ x: 10, y: 10 }];
+      this.food = { x: 5, y: 5 };
+      this.dx = 0;
+      this.dy = -1;
+      this.score = 0;
+      this.gameStarted = false;
+      this.gamePaused = false;
+      clearInterval(this.gameInterval);
+      this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+      this.drawSnake();
+      this.drawFood();
+      this.drawScore();
+    },
+    moveUp() {
+      if (!this.gamePaused && this.dy !== 1) {
+        this.dx = 0;
+        this.dy = -1;
+      }
+    },
+    moveDown() {
+      if (!this.gamePaused && this.dy !== -1) {
+        this.dx = 0;
+        this.dy = 1;
+      }
+    },
+    moveLeft() {
+      if (!this.gamePaused && this.dx !== 1) {
+        this.dx = -1;
+        this.dy = 0;
+      }
+    },
+    moveRight() {
+      if (!this.gamePaused && this.dx !== -1) {
+        this.dx = 1;
+        this.dy = 0;
+      }
+    },
+    drawSnake() {
+      this.snake.forEach(segment => {
+        this.ctx.fillStyle = "green";
+        this.ctx.fillRect(
+          segment.x * this.tileSize,
+          segment.y * this.tileSize,
+          this.tileSize,
+          this.tileSize
+        );
+      });
+    },
+    drawFood() {
+      this.ctx.fillStyle = "red";
+      this.ctx.fillRect(
+        this.food.x * this.tileSize,
+        this.food.y * this.tileSize,
+        this.tileSize,
+        this.tileSize
+      );
+    },
+    drawScore() {
+      this.ctx.fillStyle = "black";
+      this.ctx.font = "20px Arial";
+      this.ctx.fillText("Score: " + this.score, 10, 30);
+    },
+    checkCollision() {
+      const head = this.snake[0];
+      if (
+        head.x < 0 ||
+        head.x >= this.canvas.width / this.tileSize ||
+        head.y < 0 ||
+        head.y >= this.canvas.height / this.tileSize
+      ) {
+        return true;
+      }
+      for (let i = 1; i < this.snake.length; i++) {
+        if (
+          this.snake[i].x === head.x &&
+          this.snake[i].y === head.y
+        ) {
+          return true;
+        }
+      }
+      return false;
+    },
+    updateGame() {
+      if (this.checkCollision()) {
+        alert("Game Over! Score: " + this.score);
+        this.restartGame();
+        return;
+      }
+
+      const newHead = { x: this.snake[0].x + this.dx, y: this.snake[0].y + this.dy };
+      this.snake.unshift(newHead);
+
+      if (newHead.x === this.food.x && newHead.y === this.food.y) {
+        this.score += 10;
+        this.food.x = Math.floor(Math.random() * (this.canvas.width / this.tileSize));
+        this.food.y = Math.floor(Math.random() * (this.canvas.height / this.tileSize));
+      } else {
+        this.snake.pop();
+      }
+
+      this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+      this.drawSnake();
+      this.drawFood();
+      this.drawScore();
+    },
+    keyDown(event) {
+      if (!this.gamePaused) {
+        switch (event.key) {
+          case "w":
+            if (this.dy !== 1) {
+              this.dx = 0;
+              this.dy = -1;
+            }
+            break;
+          case "s":
+            if (this.dy !== -1) {
+              this.dx = 0;
+              this.dy = 1;
+            }
+            break;
+          case "a":
+            if (this.dx !== 1) {
+              this.dx = -1;
+              this.dy = 0;
+            }
+            break;
+          case "d":
+            if (this.dx !== -1) {
+              this.dx = 1;
+              this.dy = 0;
+            }
+            break;
+        }
+      }
+    },
+  },
+  created() {
+    document.addEventListener("keydown", this.keyDown);
+  },
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
+<style>
+.game-container {
+  position: relative;
+  text-align: center;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+.controls {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 10px;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+.cross-controls {
+  border-collapse: collapse;
 }
-a {
-  color: #42b983;
+
+.cross-controls td {
+  text-align: center;
+}
+
+.cross-controls button {
+  font-size: 24px;
+  padding: 5px 10px;
+  margin: 5px;
+}
+
+#gameCanvas {
+  width: 313px;
+  height: 368px;
+  border: 2px solid #000;
+}
+
+.score {
+  font-size: 20px;
 }
 </style>
